7a82d60da473078568e811f54b2dc534aa3d2def
Michael Peter Christen <mc@yacy.net>
2024-06-03 12:48:02 +0200

    added function to collect sentences from transcriptions

--- transcribe_server.py
+++ transcribe_server.py
@@ -9,6 +9,7 @@ import queue
 import torch
 import os
 import logging
+import time
 
 
 # The /transcribe endpoint expects a stream of JSON objects with base64-encoded audio binaries.
@@ -35,6 +36,7 @@ model_name = "small"    # 244M
 #model_name = "medium"   # 769M
 #model_name = "large-v3" # 1550M
 
+# load model and set mac GPU as device
 try:
     model = whisper.load_model(model_name, in_memory=True)
 except Exception as e:
@@ -42,9 +44,11 @@ except Exception as e:
     models_path = os.path.join(script_dir, 'models')
     model = whisper.load_model(model_name, in_memory=True, download_root=models_path)
 
+#model.to('mps')
+
 # In-memory storage for transcripts (you may want to use a database instead)
 transcripts = {}
-audio_stack = queue.Queue()
+audio_stack = queue.Queue() # is this a fifo queue? yes, it is, a FILO queue would be LifoQueue
 
 def process_audio():
     while True:
@@ -53,11 +57,11 @@ def process_audio():
         # Skip forward in the stack until we find the last entry with the same chunk_id
         while True:
             try:
-                next_audiob64, next_chunk_id = audio_stack.queue[0]
+                next_audiob64, next_chunk_id = audio_stack.queue[0] # peek at the first element
                 if next_chunk_id != chunk_id:
                     break
                 audiob64 = next_audiob64
-                audio_stack.get_nowait()
+                audio_stack.get_nowait() # at least one element is in the queue
             except IndexError:
                 break
 
@@ -84,18 +88,94 @@ def process_audio():
         audio_tensor = torch.from_numpy(audio_array)
 
         # Transcribe the audio data using the Whisper model
-        print("start to transcribe ...")
-        result = model.transcribe(audio_tensor)
-        print("... finished transcribe")
+        #print("start to transcribe ...")
+
+        # measure the time it takes to transcribe
+        #start_time = time.time()
+        # transcribe
+        result = model.transcribe(audio_tensor, temperature=0)
+        #print("... finished transcribe")
+        #print(f"transcribe time: {time.time() - start_time}")
         
         transcript = result['text']
-        print(transcript)
-        with threading.Lock():  # Ensure thread-safe access to shared resources
-            if chunk_id in transcripts:
-                transcripts[chunk_id]['transcript'] = transcript
-            else:
-                transcripts[chunk_id] = {'transcript': transcript}
+        if is_valid(transcript):
+            print(f"VALID transcript for chunk_id {chunk_id}: {transcript}")
+            with threading.Lock():  # Ensure thread-safe access to shared resources
+                if chunk_id in transcripts:
+                    transcripts[chunk_id]['transcript'] = transcript
+                else:
+                    transcripts[chunk_id] = {'transcript': transcript}
+        else:
+            print(f"INVALID transcript for chunk_id {chunk_id}: {transcript}")
+        
+        # clean old transcripts
+        clean_old_transcripts()
+
+        # Mark the task as done
         audio_stack.task_done()
+
+def is_valid(transcript):
+    # Check for at least one ASCII character with a code < 128
+    has_ascii_char = any(ord(char) < 128 for char in transcript)
+    
+    # Check for forbidden words (case insensitive)
+    forbidden_words = {"thank", "you", "yeah"}
+    transcript_lower = transcript.lower()
+    contains_forbidden_words = any(word in transcript_lower for word in forbidden_words)
+    
+    # Return true only if both conditions are met
+    return has_ascii_char and not contains_forbidden_words
+
+def clean_old_transcripts():
+    current_time = int(time.time() * 1000)  # Current time in milliseconds
+    two_hours_ago = current_time - (2 * 60 * 60 * 1000)  # Two hours ago in milliseconds
+    to_delete = [chunk_id for chunk_id in transcripts.keys() if int(chunk_id) < two_hours_ago]
+    for chunk_id in to_delete: del transcripts[chunk_id]
+
+def merge_and_split_transcripts(transcripts):
+
+    # Iterate through the sorted transcript keys.
+    sec = ".!?"
+    merged_transcripts = ""
+    result = {}
+    for key in transcripts.keys():
+        if not merged_transcripts:
+            # If merged_transcripts is empty, start with the first transcript.
+            merged_transcripts += transcripts[key].strip()
+        else:
+            # Append the transcript to the merged string with a space and lowercase the following first character.
+            t = transcripts[key].strip()
+            if len(t) > 1:
+                merged_transcripts += " " +  t[0].lower() + t[1:]
+            else:
+                merged_transcripts += " " + t
+
+        # find first appearance of a sentence-ending character
+        while any(char in sec for char in merged_transcripts):
+            # split the merged transcript after the first sentence-ending character
+            index = next(i for i, char in enumerate(merged_transcripts) if char in sec)
+            # get head with sentence-ending character included
+            head = merged_transcripts[:index + 1].strip()
+            head = head[0].capitalize() + head[1:] if len(head) > 1 else head
+            p = result.get(key)
+            if p:
+                result[key] = p + " " + head
+            else:
+                result[key] = head
+            
+            # get tail without sentence-ending character
+            merged_transcripts = merged_transcripts[index + 1:].strip()
+
+    # add the last part of the merged transcript
+    if merged_transcripts:
+        last_key = transcripts.keys()[-1]
+        p = result.get(last_key)
+        if p:
+            result[last_key] = p + " " + merged_transcripts
+        else:
+            result[last_key] = merged_transcripts
+
+    return result
 
 @app.route('/transcribe', methods=['POST'])
 def transcribe():
@@ -126,8 +206,11 @@ def transcribe():
 @app.route('/get_transcript', methods=['GET'])
 def get_transcript():
     chunk_id = request.args.get('chunk_id')
-    if chunk_id in transcripts:
-        return jsonify({'chunk_id': chunk_id, 'transcript': transcripts[chunk_id]['transcript']})
+    sentences = request.args.get('sentences', default='false') == 'true'
+    t = transcripts
+    if sentences == 'true': t = merge_and_split_transcripts(transcripts)
+    if chunk_id in t:
+        return jsonify({'chunk_id': chunk_id, 'transcript': t[chunk_id]['transcript']})
     else:
         return jsonify({'chunk_id': chunk_id, 'transcript': ''})
 
@@ -136,8 +219,11 @@ def get_first_transcript():
     if len(transcripts) == 0:
         return jsonify({'chunk_id': '-1', 'transcript': ''})
     else:
-        latest_chunk_id = min(transcripts.keys())
-        latest_transcript = transcripts[latest_chunk_id]['transcript']
+        sentences = request.args.get('sentences', default='false') == 'true'
+        t = transcripts
+        if sentences == 'true': t = merge_and_split_transcripts(transcripts)
+        latest_chunk_id = min(t.keys())
+        latest_transcript = t[latest_chunk_id]['transcript']
         return jsonify({'chunk_id': latest_chunk_id, 'transcript': latest_transcript})
 
 @app.route('/pop_first_transcript', methods=['GET'])
@@ -145,8 +231,11 @@ def pop_first_transcript():
     if len(transcripts) == 0:
         return jsonify({'chunk_id': '-1', 'transcript': ''})
     else:
-        latest_chunk_id = min(transcripts.keys())
-        latest_transcript = transcripts.pop(latest_chunk_id)['transcript']
+        sentences = request.args.get('sentences', default='false') == 'true'
+        t = transcripts
+        if sentences == 'true': t = merge_and_split_transcripts(transcripts)
+        latest_chunk_id = min(t.keys())
+        latest_transcript = t.pop(latest_chunk_id)['transcript']
         return jsonify({'chunk_id': latest_chunk_id, 'transcript': latest_transcript})
 
 @app.route('/get_latest_transcript', methods=['GET'])
@@ -154,8 +243,11 @@ def get_latest_transcript():
     if len(transcripts) == 0:
         return jsonify({'chunk_id': '-1', 'transcript': ''})
     else:
-        latest_chunk_id = max(transcripts.keys())
-        latest_transcript = transcripts[latest_chunk_id]['transcript']
+        sentences = request.args.get('sentences', default='false') == 'true'
+        t = transcripts
+        if sentences == 'true': t = merge_and_split_transcripts(transcripts)
+        latest_chunk_id = max(t.keys())
+        latest_transcript = t[latest_chunk_id]['transcript']
         return jsonify({'chunk_id': latest_chunk_id, 'transcript': latest_transcript})
 
 @app.route('/pop_latest_transcript', methods=['GET'])
@@ -163,26 +255,38 @@ def pop_latest_transcript():
     if len(transcripts) == 0:
         return jsonify({'chunk_id': '-1', 'transcript': ''})
     else:
-        latest_chunk_id = max(transcripts.keys())
-        latest_transcript = transcripts.pop(latest_chunk_id)['transcript']
+        sentences = request.args.get('sentences', default='false') == 'true'
+        t = transcripts
+        if sentences == 'true': t = merge_and_split_transcripts(transcripts)
+        latest_chunk_id = max(t.keys())
+        latest_transcript = t.pop(latest_chunk_id)['transcript']
         return jsonify({'chunk_id': latest_chunk_id, 'transcript': latest_transcript})
     
 @app.route('/delete_transcript', methods=['GET'])
 def delete_transcript():
     chunk_id = request.args.get('chunk_id')
-    if chunk_id in transcripts:
-        entry = transcripts.pop(chunk_id, None)
+    sentences = request.args.get('sentences', default='false') == 'true'
+    t = transcripts
+    if sentences == 'true': t = merge_and_split_transcripts(transcripts)
+    if chunk_id in t:
+        entry = t.pop(chunk_id, None)
         return jsonify({'chunk_id': chunk_id, 'transcript': entry['transcript']})
     else:
         return jsonify({'chunk_id': chunk_id, 'transcript': ''})
 
 @app.route('/list_transcripts', methods=['GET'])
 def list_transcripts():
-        return jsonify(transcripts)
+    sentences = request.args.get('sentences', default='false') == 'true'
+    t = transcripts
+    if sentences == 'true': t = merge_and_split_transcripts(transcripts)
+    return jsonify(t)
     
 @app.route('/transcripts_size', methods=['GET'])
 def transcripts_size():
-        return jsonify({'size': len(transcripts)})
+    sentences = request.args.get('sentences', default='false') == 'true'
+    t = transcripts
+    if sentences == 'true': t = merge_and_split_transcripts(transcripts)
+    return jsonify({'size': len(t)})
 
 if __name__ == '__main__':
     # Start the audio processing thread
